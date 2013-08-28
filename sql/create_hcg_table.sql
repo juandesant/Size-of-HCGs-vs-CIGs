cba8555a789eb49b8011574920ecf5e412a5ec6e
Juande Santander Vela <juandesant@gmail.com>
2013-08-28 15:11:33 -0400

    create_hcg_table.sql

---
+++ sql/create_hcg_table.sql
@@ -0,0 +1,8 @@
+USE DB SIZES_HCG_VS_CIG;
+CREATE TABLE `HCG_LVM_2001` (
+  `hcg_id` int(11) unsigned NOT NULL,
+  `ra2000` float DEFAULT NULL COMMENT 'right ascension of the HCG centre in degrees',
+  `dec2000` float DEFAULT NULL COMMENT 'declination of the HCG centre in degrees',
+  `size` float DEFAULT NULL COMMENT 'group size in arcseconds',
+  PRIMARY KEY (`hcg_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
