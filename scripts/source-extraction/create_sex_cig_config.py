8d82ec4da6d92a5845f966fa42c99304991523e8
Juande Santander Vela <juandesant@gmail.com>
2014-01-08 17:22:23 -0300

    Complete script for creating CIG-based SEXtractor files

--- scripts/source-extraction/create_sex_cig_config.py
+++ scripts/source-extraction/create_sex_cig_config.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from math import pi
+from pprint import pprint
+
 # Open data file with CIG properties
 CIG_data_file = open("xycig_mosaic.txt")
 CIG_data_lines = CIG_data_file.readlines()
@@ -55,4 +58,97 @@ for row in CIG_data:
 		transformed_row.append( (field_name, value) )
 	CIG_indexed_data.append(dict(transformed_row))
 
+def all_keywords_are_set(dictionary):
+    result = True
+    for key,value in dictionary.iteritems():
+        result = result and (value is not None)
+        if not result: break
+    return result
+
 #### Create SExtractor files for CIGs
+default_param_dict = {
+    'CATALOG_NAME':        None,
+    'PARAMETERS_NAME':     'CIG0000.param',
+    'CATALOG_TYPE':        'ASCII_HEAD', # ASCII with header
+    'DETECT_MINAREA':      None,
+    'THRESH_TYPE':         'RELATIVE',
+    'DETECT_THRESH':       1.5,    # these 2 params are changed for some CIGs
+    'ANALYSIS_THRESH':     3.0,    # these 2 params are changed for some CIGs
+    'FILTER':              'Y',
+    'FILTER_NAME':         'default.conv', # default convolution kernel
+    'DEBLEND_NTHRESH':     1,
+    'DEBLEND_MINCONT':     0.000005,
+    'CLEAN':               'Y',
+    'CLEAN_PARAM':         1.0,
+    'MAG_ZEROPOINT':       22.5,
+    'PHOT_APERTURES':      (0.1, 0.2, 0.4, 0.6, 0.8, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0, 21.0, 22.0, 23.0, 24.0),
+    'PHOT_AUTOPARAMS':     (2.5,1.5),
+    'MASK_TYPE':           'CORRECT',
+    'DETECT_TYPE':         'CCD',
+    'PIXEL_SCALE':         0.396, # default SDSS pixel scale
+    'SATUR_LEVEL':         10.,
+    'GAIN':                1.0,
+    'MAG_GAMMA':           4.0,
+    'SEEING_FWHM':         1.0,
+    'STARNNW_NAME':        'default.nnw',
+    'BACK_SIZE':           64,
+    'BACK_FILTERSIZE':     3,
+    'BACK_TYPE':           'MANUAL',
+    'BACK_VALUE':          (0.0,0.0),
+    'BACKPHOTO_TYPE':      'GLOBAL',
+    'BACKPHOTO_THICK':     24,
+    'CHECKIMAGE_TYPE':     'OBJECTS',
+    'CHECKIMAGE_NAME':     None,
+    'MEMORY_OBJSTACK':     3000,
+    'MEMORY_PIXSTACK':     5000000,
+    'MEMORY_BUFSIZE':      1024,
+    'VERBOSE_TYPE':        'NORMAL',
+    'PHOT_PETROPARAMS':    (2.0,3.5),
+    'PHOT_FLUXFRAC':       (0.5,1.0)
+}
+
+for cig_record in CIG_indexed_data:
+    # get a copy of the default parameters
+    # if we did param_dict = default_param_dict instead,
+    # we would be changing elements of default_param_dict
+    # by changing param_dict
+    param_dict = default_param_dict.copy()
+    sex_file_name = "CIG%04d_g.sex" % cig_record['CIG']
+    cig_number = cig_record['CIG']
+    param_dict['CATALOG_NAME'] = cig_record['imagen']+'.cat'
+    # object-dependent DETECT_MINAREA
+    radius = (cig_record['side']*60./8.)/default_param_dict['PIXEL_SCALE']
+    miniarea = (radius * radius * pi) * 0.05
+    if cig_number == 2 or cig_number == 566:
+        miniarea = miniarea/2.
+    param_dict['DETECT_MINAREA'] = miniarea
+    # object-dependent threholds for detection/analysis;
+    # the default is already set before
+    if cig_number in (691, 971):
+        param_dict['DETECT_THRESH']   = 0.25
+        param_dict['ANALYSIS_THRESH'] = 0.5
+    if cig_number in (322, 974):
+        param_dict['DETECT_THRESH']   = 3.75
+        param_dict['ANALYSIS_THRESH'] = 3.5
+    if cig_number in (304, 291, 397, 484, 470, 224):
+        param_dict['DETECT_THRESH']   = 5.0
+        param_dict['ANALYSIS_THRESH'] = 3.5
+    # size-dependent photo-aperture
+    param_dict['PHOT_APERTURES'] = tuple(map(
+                                    lambda x: x*radius/10.,
+                                    default_param_dict['PHOT_APERTURES']
+    ))
+    param_dict['CHECKIMAGE_NAME'] = cig_record['imagen']+'seg.fits'
+    # prepare for writing; file is closed at the end of the with block
+    if not all_keywords_are_set(param_dict):
+        print "Not all keywords set"
+        pprint(param_dict)
+        break
+    print "Writing %s" % sex_file_name
+    with open(sex_file_name,"w") as sex_file:
+        for key,value in param_dict.iteritems():
+            file_value = str(value)
+            if type(value) == type((1,)):
+                file_value = str(value)[1:-1] # removing parentheses
+            sex_file.write("%-20s%s\n" % (key,file_value))
+            print("%-20s%s" % (key,file_value))
